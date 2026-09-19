bbda54e179639794bf685f518419d7cea4228319
reza2557 <mohamadreza.hooshiarzadeh@gmail.com>
2026-09-19 13:52:15 +0330

    Add files via upload

---
+++ postchi-app/postchi-app/sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'postchi-v1';
+const SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  // Only cache-first for same-origin app shell files. Everything else
+  // (Neshan/Nominatim/Tesseract CDN calls) goes straight to the network.
+  if (url.origin === self.location.origin) {
+    e.respondWith(
+      caches.match(e.request).then((cached) => cached || fetch(e.request))
+    );
+  }
+});
